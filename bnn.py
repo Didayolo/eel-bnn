More documentation and bias computation corrected.

bnn.py
@@ -7,18 +7,17 @@ from activations import sigmoid, tanh, relu, softmax, sign
 
 class BNN():
 
-    def __init__(self, layers=[10, 10, 10], act_func_name='sign', W=None, B=None):
-        """
-        Feed-forward Binary Neural Network.
-        Initialize the neural network weights, activation function and return the number of parameters
-        Inputs: layers: the number of units per layer -  list of int
-              : act_func_name: the activation function name (sigmoid, tanh or relu) - str
-        Outputs: W: a list of weights for each hidden layer - list of ndarray
-               : B: a list of bias for each hidden layer - list of ndarray
-               : act_func: the activation function - function
+    def __init__(self, layers=[10, 10, 10], W=None, B=None):
+        """ Feed-forward Binary Neural Network.
+
+            Initialize the neural network weights, activation function and return the number of parameters
+
+            :param layers: The number of units per layer - list of int
+            :param W: List of weight matrices, None for random initialization.
+            :param B: List of bias matrices, None for random initialization.
         """
         self.layers = layers # architecture
-        self.act_func = globals()[act_func_name]  # Cast the string to a function
+        self.act_func = sign  # Use sign activation function
         if W is None:
             W = []
             for i in range(np.size(layers) - 1):
@@ -35,30 +34,32 @@ class BNN():
         self.B = B
 
     def predict_proba(self, X):
+        """ Perform the forward propagation.
+
+            :param X: The batch - np.ndarray
+            :return: A list of activation values - list of np.ndarray
         """
-        Perform the forward propagation
-        Inputs: X: the batch - ndarray
-        Outputs: a list of activation values - list of ndarray
-        """
-        if not isinstance(X, np.ndarray):
-            X = np.array(X)
         X = X.T
         for i in range(len(self.W) - 1):
-            X = self.act_func(np.dot(self.W[i], X))# + self.B[i])
-        X = softmax(np.dot(self.W[-1], X)) # + self.B[-1] # softmax on last layer
+            X = self.act_func(np.dot(self.W[i], X) + self.B[i])
+        X = softmax(np.dot(self.W[-1], X) + self.B[-1]) # softmax on last layer
         return X.T
 
     def predict(self, X):
+        """ Perform the forward propagation and output the argmax target.
+
+            :param X: Batch of data - np.ndarray
+            :return: Predicted target index
+        """
         y_pred = self.predict_proba(X)
         return np.argmax(y_pred, axis=1)
 
     def loss(self, X, y_true):
-        """
-        Compute the loss value of the current network on the full batch
-        Inputs: X: the batch - ndarray
-              : y_true: the labels corresponding to the batch
-        Outputs: loss: the negative log-likelihood - float
-               : accuracy: the ratio of examples that are well-classified - float
+        """ Compute the loss value of the current network on the full batch
+
+            :param X: Batch of data - np.ndarray
+            : y_true: Labels corresponding to the batch - np.ndarray
+            :return: The negative log-likelihood - float
         """
         y_pred = self.predict_proba(X)
         loss = 0
@@ -69,8 +70,8 @@ class BNN():
 
 if __name__ == "__main__":
     model = BNN(layers=[2, 4])
-    X = [[1, 3], [0, -3]]
-    y = [0, 1]
+    X = np.array([[1, 3], [0, -3]])
+    y = np.array([0, 1])
     print('layers')
     print(model.layers)
     print('bias')

eel.py
@@ -16,6 +16,8 @@ class EEL():
 
             Method used by default: Post-Mortem Ensemble Creation.
 
+            :param layers: List of integers representing the size of each layer.
+                           The first is the input size and the last is the output size.
             :param n_estimators: Number of estimators of the ensemble method.
                                  This also the size of the parent population ("mu").
             :param l: The size of the offsprings during training ("lambda").
@@ -64,34 +66,60 @@ class EEL():
             return new_population + self.population
         return new_population
 
-    def selection(self, X, y, new_population, batch_size=250, replace=True):
+    def selection(self, X, y, new_population, batch_size=250, replace=True, verbose=False):
         """ Select best models from population.
+
+            :param X: np.ndarray of data
+            :param y: np.ndarray of target corresponding to data
+            :param new_population: The population to select individuals from.
+            :param batch_size: Size of sub-samples of data to feed models with.
+            :param replace: If True, sample batches with replacement.
+            :param verbose: If True, display information about loss.
         """
         losses = []
         for model in new_population:
             # TODO: boostrap? random batch?
             idx = np.random.choice(np.arange(len(X)), batch_size, replace=replace)
-            X_sampled = X[idx] # TODO: do this computation less times
+            X_sampled = X[idx] # TODO: this is not efficient
             y_sampled = y[idx]
             losses.append(model.loss(X_sampled, y_sampled))
         losses = np.array(losses)
-        #print(np.sum(losses)) # generation's total loss # TODO: learning curve?
         best = losses.argsort()[:self.n_estimators+1] # select model with lowest losses
-        #print('loss = {}'.format(np.sum(np.extract(best, losses)) / self.n_estimators)) # selected generation's loss
+        if verbose:
+            #print(np.sum(losses)) # generation's total loss # TODO: learning curve?
+            print('loss = {}'.format(np.sum(np.extract(best, losses)) / self.n_estimators)) # selected generation's loss
         return list(np.extract(best, new_population)) # populations only of type ndarray to avoid too many castings?
 
-    def fit(self, X, y, epochs=1, p=0.2, keep=False, batch_size=250, replace=True):
+    def fit(self, X, y, epochs=1, p=0.2, constant_p=True, keep=False, batch_size=250, replace=True, power_t=0.2, verbose=False):
         """ Evolve to a new generation of estimators.
+
+            :param X: Data
+            :param y: Target corresponding to data
+            :param epochs: Number of learning epochs.
+            :param p: Initial probability of mutation (kind of "learning rate") during variation phase.
+            :param constant_p: If True, keeps p constant (the learning rate).
+            :param keep: If True, the current population is kept during the variation phase.
+            :param batch_size: Size of sub-samples of data to feed models with during selection phase.
+            :param replace: If True, sample batches with replacement during selection phase.
+            :param power_t: Factor for invscaling learning rate.
+            :param verbose: If True, display information about loss and learning rate evolution.
         """
+        if not isinstance(X, np.ndarray):
+            X = np.array(X)
+        if not isinstance(y, np.ndarray):
+            y = np.array(y)
         # TODO: clean dynamic learning rate
-        decrease = 0.05
+        # adaptive: keeps the learning rate constant to ‘learning_rate_init’ as long as training loss keeps decreasing.
+        p_init = p # intial value of learning rate
         for i in range(epochs):
-            # decrease mutation probability during training (learning rate)
-            if p > 0.001:
-                p -= decrease / (i+1)
-                #print('p = {}'.format(p))
+            if not constant_p:
+                # decrease mutation probability during training (learning rate)
+                # invscaling:
+                p = p_init / pow(i+1, power_t)
+                if verbose:
+                    print('p = {}'.format(p))
             new_population = self.variation(p=p, keep=keep)
-            self.population = self.selection(X, y, new_population, batch_size=batch_size, replace=replace)
+            self.population = self.selection(X, y, new_population, batch_size=batch_size, replace=replace, verbose=verbose)
             self.generation += 1
 
     def predict_proba(self, X, soft=True, proba=True):
@@ -115,6 +143,11 @@ class EEL():
         return sum
 
     def predict(self, X, soft=True):
+        """ Predict X's target with a majority vote between (trained) estimators.
+
+            :param X: np.ndarray of data
+            :param soft: If True do a soft vote, else do a hard vote.
+        """
         y_pred = self.predict_proba(X, soft=soft, proba=False)
         return np.argmax(y_pred, axis=1) # take the most voted label
 